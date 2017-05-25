af26141e21e8508184f2c13e3be209d5691894da
Gabriel West <gabe_west1@yahoo.com>
2017-05-25 15:40:24 -0500

    Added `Navbar` to `App` container

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 
 import TwitchUserStatusList from "../TwitchUserStatusList"
+import Navbar from "../Navbar"
 import Container from "./Container"
 
 export default class App extends React.Component {
     render() {
         return (
             <Container>
+                <Navbar />
                 <TwitchUserStatusList />
             </Container>
         )
